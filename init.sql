47f80b062b3e92adb22b6b3190f10cc6dff5c0fc
chelseaerinavery <chelsea.erin.avery@gmail.com>
2022-02-16 19:48:07 -0700

    deployed

--- init.sql
+++ init.sql
@@ -2,5 +2,5 @@ CREATE TABLE todos (
   id SERIAL PRIMARY KEY,
   name varchar(255) DEFAULT NULL,
   description varchar(255) DEFAULT NULL,
-  completed SMALLINT DEFAULT 0
+  completed BOOLEAN DEFAULT FALSE
 );
