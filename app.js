f55676c25b136d98d3c73df3bb56d0048374e9af
chelseaerinavery <chelsea.erin.avery@gmail.com>
2022-02-16 17:36:53 -0700

    changed port syntax

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { pool } = require("./config");
 var cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 // var pool = mysql.createpool({
